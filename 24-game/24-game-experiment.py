04ed2e1bc1d5fd0a7ce211af9a0a1330ab88dd91
Franklin <franklinjiachen@gmail.com>
2024-06-09 15:14:23 -0700

    if b!= 0 else 1e9 => "if b else 0" (-6 chars)
    if b is smart to avoid division by 0 but letting it equal to 0 does not mess up the 24 game

--- 24-game/24-game-experiment.py
+++ 24-game/24-game-experiment.py
@@ -20,16 +20,14 @@ def print_if_make_24(nums):
 for comb in combinations_with_replacement(range(1, 14), 4):
     print_if_make_24(comb)
 
-# from itertools import combinations_with_replacement as p,permutations as q
-# def h(a,b):return [k for i in a for j in b for k in (i+j,i-j,i*j,i/j if j else 1e9)]
-# def j(n):
-#     for e,f,g,z in q(n):
-#         a = [e]
-#         b = [f]
-#         c = [g]
-#         d = [z]
-#         pos_values = h(h(h(a,b),c),d) + h(h(a,b),h(c,d)) + h(h(a,h(b,c)),d) + h(a,h(h(b,c),d)) + h(a,h(b,h(c,d)))
-#         if 24 in pos_values or (3,3,8,8)==(e,f,g,z):
-#             print(*n)
-#             return
-# for i in p(range(1,14),4):j(i)
+from itertools import combinations_with_replacement as p,permutations as q
+h=lambda a, b:[k for i in a for j in b for k in (i+j, i-j, i*j, i/j if j else 0)]
+def j(n):
+	for e,f,g,z in q(n):
+		a = [e]
+		b = [f]
+		c = [g]
+		d = [z]
+		pos_values = h(h(h(a,b),c),d) + h(h(a,b),h(c,d)) + h(h(a,h(b,c)),d) + h(a,h(h(b,c),d)) + h(a,h(b,h(c,d)))
+		if 24 in pos_values or (3,3,8,8)==(e,f,g,z):print(*n);return
+for i in p(range(1,14),4):j(i)
