275878f7fff9913ba1cd52c63738cea9ee7523ac
Franklin <franklinjiachen@gmail.com>
2024-06-09 16:03:53 -0700

    Save edgecase handling
    (3,3,8,8)==(e,f,g,z) --> 8/(3-8/3) in v

--- 24-game/24-game-golf.py
+++ 24-game/24-game-golf.py
@@ -4,5 +4,5 @@ def j(n):
 	for e,f,g,z in q(n):
 		a,b,c,d=[e],[f],[g],[z]
 		v=h(h(h(a,b),c),d)+h(h(a,b),h(c,d))+h(h(a,h(b,c)),d)+h(a,h(h(b,c),d))+h(a,h(b,h(c,d)))
-		if 24 in v or (3,3,8,8)==(e,f,g,z):print(*n);return
+		if 24 or 8/(3-8/3) in v:print(*n);return
 for i in p(range(1,14),4):j(i)
