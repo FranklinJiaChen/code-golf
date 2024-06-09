7a4a7736b99557a517832f30666bff3daee48473
Franklin <franklinjiachen@gmail.com>
2024-06-09 15:03:57 -0700

    spaces --> tabs (-18 chars)

--- 24-game/24-game-experiment.py
+++ 24-game/24-game-experiment.py
@@ -1,6 +1,6 @@
 from itertools import combinations_with_replacement, permutations, product
 
-def return_possible_values(a: list[int], b: list[int]):
+def h(a: list[int], b: list[int]):
     """
     Return the possible values that a and b can produce by +, -, *, or /
     """
@@ -12,10 +12,24 @@ def print_if_make_24(nums):
         b = [f]
         c = [g]
         d = [h]
-        pos_values = return_possible_values(return_possible_values(return_possible_values(a, b), c), d) + return_possible_values(return_possible_values(a, b),return_possible_values(c, d)) + return_possible_values(return_possible_values(a, return_possible_values(b, c)), d) + return_possible_values(a, return_possible_values(return_possible_values(b, c), d)) + return_possible_values(a, return_possible_values(b, return_possible_values(c, d)))
+        pos_values = h(h(h(a, b), c), d) + h(h(a, b),h(c, d)) + h(h(a, h(b, c)), d) + h(a, h(h(b, c), d)) + h(a, h(b, h(c, d)))
         if 24 in pos_values or (3,3,8,8)==(e,f,g,h):
             print(*nums)
             return
 
 for comb in combinations_with_replacement(range(1, 14), 4):
     print_if_make_24(comb)
+
+# from itertools import combinations_with_replacement as p,permutations as q
+# def h(a,b):return [k for i in a for j in b for k in (i+j,i-j,i*j,i/j if j else 1e9)]
+# def j(n):
+#     for e,f,g,z in q(n):
+#         a = [e]
+#         b = [f]
+#         c = [g]
+#         d = [z]
+#         pos_values = h(h(h(a,b),c),d) + h(h(a,b),h(c,d)) + h(h(a,h(b,c)),d) + h(a,h(h(b,c),d)) + h(a,h(b,h(c,d)))
+#         if 24 in pos_values or (3,3,8,8)==(e,f,g,z):
+#             print(*n)
+#             return
+# for i in p(range(1,14),4):j(i)
--- 24-game/24-game-golf.py
+++ 24-game/24-game-golf.py
@@ -1,6 +1,6 @@
 from itertools import combinations_with_replacement as p,permutations as q,product as r
 def j(n):
-    for a,b,c,d in q(n):
-        for e,f,g in r([lambda a,b:a+b,lambda a,b:a-b,lambda a,b:a*b,lambda a,b:a/b if b!= 0 else 1e9],repeat=3):
-            if any((abs(x-24)<1e-6) for x in [g(f(e(a,b),c),d),g(e(a,b),f(c,d)),g(f(a,e(b,c)),d),g(a,f(e(b,c),d)),g(a,f(b,e(c,d)))]):print(*n);return
+	for a,b,c,d in q(n):
+		for e,f,g in r([lambda a,b:a+b,lambda a,b:a-b,lambda a,b:a*b,lambda a,b:a/b if b!= 0 else 1e9],repeat=3):
+			if any((abs(x-24)<1e-6) for x in [g(f(e(a,b),c),d),g(e(a,b),f(c,d)),g(f(a,e(b,c)),d),g(a,f(e(b,c),d)),g(a,f(b,e(c,d)))]):print(*n);return
 for i in p(range(1,14),4):j(i)
