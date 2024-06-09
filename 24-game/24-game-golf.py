810c54750988a1fb80853c66e5630dfe5db28939
Franklin <franklinjiachen@gmail.com>
2024-06-09 16:12:08 -0700

    Improve (11 bytes)
    "j==0 or i/j" => "j and i/j". Smarter short circuit evaluation (2 bytes)
    
    remove " as _" * 2 with * in import (9 bytes)
    
    also replaces tabs with spaces.
    
    improved with ChatGPT

--- 24-game/24-game-golf.py
+++ 24-game/24-game-golf.py
@@ -1,8 +1,8 @@
-from itertools import combinations_with_replacement as p,permutations as q
-h=lambda a,b:[k for i in a for j in b for k in (i+j,i-j,i*j,j==0 or i/j)]
+from itertools import*
+h=lambda a,b:[k for i in a for j in b for k in(i+j,i-j,i*j,j and i/j)]
 def j(n):
-	for e,f,g,z in q(n):
-		a,b,c,d=[e],[f],[g],[z]
-		v=h(h(h(a,b),c),d)+h(h(a,b),h(c,d))+h(h(a,h(b,c)),d)+h(a,h(h(b,c),d))+h(a,h(b,h(c,d)))
-		if 24 or 8/(3-8/3) in v:print(*n);return
-for i in p(range(1,14),4):j(i)
+ for e,f,g,z in permutations(n):
+  a,b,c,d=[e],[f],[g],[z]
+  v=h(h(h(a,b),c),d)+h(h(a,b),h(c,d))+h(h(a,h(b,c)),d)+h(a,h(h(b,c),d))+h(a,h(b,h(c,d)))
+  if 24 in v or 8/(3-8/3) in v:print(*n);return
+for i in combinations_with_replacement(range(1,14),4):j(i)
