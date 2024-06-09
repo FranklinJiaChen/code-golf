d4f99f899d63e2350813e4974a0259b3cf0b2214
Franklin <franklinjiachen@gmail.com>
2024-06-09 16:41:07 -0700

    Delete h(h(a,h(b,c)),d); generates no new solutions
    remove h(h(a,h(b,c)),d) (17 bytes)

--- 24-game/24-game-golf.py
+++ 24-game/24-game-golf.py
@@ -3,6 +3,6 @@ h=lambda a,b:[k for i in a for j in b for k in(i+j,i-j,i*j,j and i/j)]
 def j(n):
  for e,f,g,z in permutations(n):
   a,b,c,d=[e],[f],[g],[z]
-  v=h(h(h(a,b),c),d)+h(h(a,b),h(c,d))+h(h(a,h(b,c)),d)+h(a,h(h(b,c),d))+h(a,h(b,h(c,d)))
+  v=h(h(h(a,b),c),d)+h(h(a,b),h(c,d))+h(a,h(h(b,c),d))+h(a,h(b,h(c,d)))
   if 24 in v or 8/(3-8/3) in v:print(*n);return
 for i in combinations_with_replacement(range(1,14),4):j(i)
