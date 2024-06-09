7b2873297d00708797789e7ec996f03b22bbeaf2
Franklin <franklinjiachen@gmail.com>
2024-06-09 13:46:27 -0700

    Shorten variable and remove indent (448)

--- 24-game/24-game-golf.py
+++ 24-game/24-game-golf.py
@@ -1,7 +1,6 @@
 from itertools import combinations_with_replacement as p,permutations as q,product as r
-def j(nums):
-    for a, b, c, d in q(nums):
+def j(n):
+    for a, b, c, d in q(n):
         for e, f, g in r([lambda a,b:a+b,lambda a,b:a-b, lambda a,b:a*b, lambda a,b:a/b if b!= 0 else 1e9], repeat=3):
-            if any((abs(expr-24)<1e-6) for expr in [g(f(e(a, b), c), d),g(e(a, b), f(c, d)),g(f(a, e(b, c)), d),g(a, f(e(b, c), d)),g(a, f(b, e(c, d)))]):print(*nums);return
-for i in p(range(1, 14), 4):
-    j(i)
+            if any((abs(expr-24)<1e-6) for expr in [g(f(e(a, b), c), d),g(e(a, b), f(c, d)),g(f(a, e(b, c)), d),g(a, f(e(b, c), d)),g(a, f(b, e(c, d)))]):print(*n);return
+for i in p(range(1, 14), 4):j(i)
