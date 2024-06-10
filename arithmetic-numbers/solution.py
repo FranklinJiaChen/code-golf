3190975e542ea7f7d28e263295e14d6e631d29fe
Franklin <franklinjiachen@gmail.com>
2024-06-10 14:28:34 -0700

    sum%len checks if the average is an integer
    chatGPT idea

--- arithmetic-numbers/solution.py
+++ arithmetic-numbers/solution.py
@@ -1,4 +1,1 @@
-for n in range(1,10000):
-	l=[i for i in range(1,n+1) if n%i<1]
-	a=sum(l)/len(l)
-	if a//1==a:print(n)
+for n in range(1,10000):a=[i for i in range(1,n+1) if n%i<1];sum(a)%len(a)<1and print(n)
