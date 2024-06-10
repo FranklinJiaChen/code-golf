66de9906fd610143123901dddc6dd6698bc16f31
Franklin <franklinjiachen@gmail.com>
2024-06-09 17:52:56 -0700

    Get rid of function only called once (14 bytes)

--- 24-game/24-game-golf.py
+++ 24-game/24-game-golf.py
@@ -1,8 +1,7 @@
 from itertools import*
 h=lambda a,b:[k for i in a for j in b for k in(i+j,i-j,i*j,j and i/j)]
-def j(n):
- for e,f,g,z in permutations(n):
+for i in combinations_with_replacement(range(1,14),4):
+ for e,f,g,z in permutations(i):
   a,b,c,d=[e],[f],[g],[z]
   v=h(h(h(a,b),c),d)+h(h(a,b),h(c,d))+h(a,h(h(b,c),d))+h(a,h(b,h(c,d)))
-  if 24 in v or 8/(3-8/3) in v:print(*n);break
-for i in combinations_with_replacement(range(1,14),4):j(i)
+  if 24 in v or 8/(3-8/3) in v:print(*i);break
