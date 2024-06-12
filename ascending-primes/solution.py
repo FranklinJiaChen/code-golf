9fcabd48d190835a2d5d5317083c12a8e2097f97
Franklin <franklinjiachen@gmail.com>
2024-06-11 21:31:21 -0700

    (-5 bytes) Use modular logic to get rid of hardcoded print(2)
    I'm proud of this trick

--- ascending-primes/solution.py
+++ ascending-primes/solution.py
@@ -1,6 +1,5 @@
-i=3
-print(2)
+i=2
 while i<24e6:
 	s=str(i)
-	if len(set(s))==len(s)and list(s)==sorted(s)and all(i%j for j in range(2,int(i**0.7))):print(i)
-	i+=2
+	if len(set(s))==len(s)and list(s)==sorted(s)and all(i%j for j in range(2, int(i**0.7))):print(i)
+	i+=i%2+1
