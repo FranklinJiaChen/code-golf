fded2a45d97fb3448c08b0461067190d7435e1a1
Franklin <franklinjiachen@gmail.com>
2024-06-11 19:10:47 -0700

    Remove 1*

--- arrows/solution_byte.py
+++ arrows/solution_byte.py
@@ -1,4 +1,4 @@
 import sys
 x=y=0
 d='↙↖←↲↰⇐⇙⇖⇦↘↗→↳↱⇒⇘⇗⇨'
-for a in sys.argv[1:]:x-=1*(a in d)-2*(a in d[9:]);y+=(a in d[1::3]+'↑⇑⇧')-(a in d[::3]+'↓⇓⇩');print(x,y)
+for a in sys.argv[1:]:x-=(a in d)-2*(a in d[9:]);y+=(a in d[1::3]+'↑⇑⇧')-(a in d[::3]+'↓⇓⇩');print(x,y)
