f526c02fbb319ab945558e41e5f5606c5f8a7611
Franklin <franklinjiachen@gmail.com>
2024-06-11 21:23:32 -0700

    (-22 bytes) Use variable for str(i) and all(). This lets program fast enough to complete before i**0.7 rather than needing **0.5 then adding 1

--- ascending-primes/solution.py
+++ ascending-primes/solution.py
@@ -1,5 +1,6 @@
 i=3
 print(2)
 while i<24e6:
-	if len(set(str(i)))==len(str(i))and list(str(i))==sorted(str(i))and(len([j for j in range(2,int(i**0.5)+1)if i%j<1])<1):print(i)
+	s=str(i)
+	if len(set(s))==len(s)and list(s)==sorted(s)and all(i%j for j in range(2,int(i**0.7))):print(i)
 	i+=2
