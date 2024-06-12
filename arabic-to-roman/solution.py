9b552220bbba467a23fc54b90bdb09fd3324be77
Franklin <franklinjiachen@gmail.com>
2024-06-12 16:21:46 -0700

    Update arabic-to-roman, remove Parenthesis

--- arabic-to-roman/solution.py
+++ arabic-to-roman/solution.py
@@ -1,5 +1,5 @@
 import sys
 for z in sys.argv[1:]:
 	y=''
-	for e,f in zip([1000,900,500,400,100,90,50,40,10,9,5,4,1],"M CM D CD C XC L XL X IX V IV I".split()):n,z=divmod(int(z),e);y+=(f*n)
+	for e,f in zip([1000,900,500,400,100,90,50,40,10,9,5,4,1],"M CM D CD C XC L XL X IX V IV I".split()):n,z=divmod(int(z),e);y+=f*n
 	print(y)
