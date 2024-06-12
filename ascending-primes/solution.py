b285063a73c95f470d6d97d451bb0ac5ab8d265d
Franklin <franklinjiachen@gmail.com>
2024-06-11 21:15:06 -0700

    Hardcode 2 and optimize runtime to not need hardcode bottom 2 (-13 bytes)

--- ascending-primes/solution.py
+++ ascending-primes/solution.py
@@ -1,6 +1,5 @@
-i=2
-while i<2e6:
-	if len(set(str(i)))==len(str(i))and list(str(i))==sorted(str(i))and(len([j for j in range(2, i-1)if i%j<1])<1):print(i)
-	i+=1
-print(12356789)
-print(23456789)
+i=3
+print(2)
+while i<24e6:
+	if len(set(str(i)))==len(str(i))and list(str(i))==sorted(str(i))and(len([j for j in range(2,int(i**0.5)+1)if i%j<1])<1):print(i)
+	i+=2
