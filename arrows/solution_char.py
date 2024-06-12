e10bf71b32ec92b64c7966cb80602da4047ce7ed
Franklin <franklinjiachen@gmail.com>
2024-06-11 19:06:33 -0700

    remove spaces when " in ' " (-4 chars)

--- arrows/solution_char.py
+++ arrows/solution_char.py
@@ -1,3 +1,3 @@
 import sys
 x=y=0
-for a in sys.argv[1:]:x+=(a in '↘↳⇘→⇒⇨↗↱⇗')-(a in '↙↲⇙←⇐⇦↖↰⇖');y+=(a in '↖↰⇖↑⇑⇧↗↱⇗')-(a in '↙↲⇙↓⇓⇩↘↳⇘');print(x,y)
+for a in sys.argv[1:]:x+=(a in'↘↳⇘→⇒⇨↗↱⇗')-(a in'↙↲⇙←⇐⇦↖↰⇖');y+=(a in'↖↰⇖↑⇑⇧↗↱⇗')-(a in'↙↲⇙↓⇓⇩↘↳⇘');print(x,y)
