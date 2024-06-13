ae77e16366d3a0ada443259ceb1f9995d74d2cb8
Franklin <franklinjiachen@gmail.com>
2024-06-13 11:16:55 -0700

    [-2 bytes] ascending prime: replace set(s) with {*s}
    Splat on iterables
    
    strategy on codegolf wiki

--- ascending-primes/solution.py
+++ ascending-primes/solution.py
@@ -1,6 +1,6 @@
 i=2
 while i<13e6:
 	s=str(i)
-	if "".join(sorted(set(s)))==s and all(i%j for j in range(3,i)):print(i)
+	if "".join(sorted({*s}))==s and all(i%j for j in range(3,i)):print(i)
 	i+=i%2+1
 print(23456789)
