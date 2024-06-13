144318b7a63377ab946cf235c6b3bbddf9e04e99
Franklin <franklinjiachen@gmail.com>
2024-06-12 22:13:35 -0700

    (137 bytes)
    result made by a ChatGPT suggestion

--- ascii-table/solution.py
+++ ascii-table/solution.py
@@ -1,3 +1,2 @@
-print('   2 3 4 5 6 7\n',"-"*13)
-for i in range(16):
-    print('0123456789ABCDEF'[i]+':',chr(32+i),chr(48+i),chr(64+i),chr(80+i),chr(96+i),[chr(112+i),'DEL'][i>14])
+print('   2 3 4 5 6 7\n '+'-'*13)
+for i in range(16):print(f'{i:X}: {" ".join(chr(32+i+j*16)for j in range(6)).replace(chr(127),"DEL")}')
