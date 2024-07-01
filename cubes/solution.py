0243687a61feb1874311e56f2bdfd8baaacd6ccb
Franklin <franklinjiachen@gmail.com>
2024-07-01 11:35:09 -0700

    -24, assign print and range to variable

--- cubes/solution.py
+++ cubes/solution.py
@@ -1,8 +1,8 @@
-s,o,h,v,d=' ','█','─','│','╱'
-for n in range(1,8):
-	a=s*(n*4);print(s*(n+1)+o+h*(n*4)+o);b=v+a+v
-	for i in range(n):print(s*(n-i)+d+a+d+" "*i+v)
-	print(o+h*(n*4)+o+(s*n)+v)
-	for i in range(n):print(b+s*n+[v,o][i+1==n])
-	for i in range(n):print(b+s*(n-i-1)+d)
-	print(o+h*(n*4)+o+'\n')
+s,o,h,v,d,r,p=' ','█','─','│','╱',range,print
+for n in r(1,8):
+	a=s*(n*4);p(s*(n+1)+o+h*(n*4)+o);b=v+a+v
+	for i in r(n):p(s*(n-i)+d+a+d+" "*i+v)
+	p(o+h*(n*4)+o+(s*n)+v)
+	for i in r(n):p(b+s*n+[v,o][i+1==n])
+	for i in r(n):p(b+s*(n-i-1)+d)
+	p(o+h*(n*4)+o+'\n')
