289c4c9369af00f0a661728d4ef2c4bef52b4e17
Franklin <franklinjiachen@gmail.com>
2024-07-02 16:59:06 -0700

    -2 bytes: distribute s*(n+1) into s*n+s

--- cubes/solution.py
+++ cubes/solution.py
@@ -1,5 +1,5 @@
 s,o,v,d,p=' ','█','│','╱',print
 for n in range(1,8):
-	a=s*(n*4);c=o+'─'*(n*4)+o;p(s*(n+1)+c)
+	a=s*(n*4);c=o+'─'*(n*4)+o;p(s*n+s+c)
 	for i in range(3*n):p([s*(n-i)+d+a+d+" "*i+v,(c+s*n+v+'\n')*(i==n)+v+a+v+s*n+[v,o][i>2*n-2],v+a+v+s*(3*n-i-1)+d][i//n])
 	p(c);p()
