88b902dfea9a08066afc5a82c49716d047c9a1ce
Franklin <franklinjiachen@gmail.com>
2024-07-02 16:01:38 -0700

    Reverse inequality ordering to save a byte

--- cubes/solution.py
+++ cubes/solution.py
@@ -3,5 +3,5 @@ for n in r(1,8):
 	a=s*(n*4);b=v+a+v;p(s*(n+1)+o+h*(n*4)+o)
 	for i in r(n):p(s*(n-i)+d+a+d+" "*i+v)
 	p(o+h*(n*4)+o+(s*n)+v)
-	for i in r(2*n):p([b+s*n+[v,o][i+1==n],b+s*(2*n-i-1)+d][i>=n])
+	for i in r(2*n):p([b+s*(2*n-i-1)+d,b+s*n+[v,o][i+1==n]][i<n])
 	p(o+h*(n*4)+o+'\n')
