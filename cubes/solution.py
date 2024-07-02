a8fe29bbfa92ef0b4b86e86645d27a43163f0ffb
Franklin <franklinjiachen@gmail.com>
2024-07-02 16:52:53 -0700

    -4 byte: remove unnecessary variable h
    also remove other variables that have same char count with or without

--- cubes/solution.py
+++ cubes/solution.py
@@ -1,5 +1,5 @@
-s,o,h,v,d,r,p=' ','█','─','│','╱',range,print
-for n in r(1,8):
-	a=s*(n*4);b,c=v+a+v,o+h*(n*4)+o;p(s*(n+1)+c)
-	for i in r(3*n):p([s*(n-i)+d+a+d+" "*i+v,(c+s*n+v+'\n')*(i==n)+b+s*n+[v,o][i>2*n-2],b+s*(3*n-i-1)+d][i//n])
+s,o,v,d,p=' ','█','│','╱',print
+for n in range(1,8):
+	a=s*(n*4);c=o+'─'*(n*4)+o;p(s*(n+1)+c)
+	for i in range(3*n):p([s*(n-i)+d+a+d+" "*i+v,(c+s*n+v+'\n')*(i==n)+v+a+v+s*n+[v,o][i>2*n-2],v+a+v+s*(3*n-i-1)+d][i//n])
 	p(c);p()
