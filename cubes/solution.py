b9856af374e1a7986aeaeb2ef6fc7f993c26766c
Franklin <franklinjiachen@gmail.com>
2024-07-02 16:14:05 -0700

    -16 bytes let double vertex print be a variable

--- cubes/solution.py
+++ cubes/solution.py
@@ -1,7 +1,7 @@
 s,o,h,v,d,r,p=' ','█','─','│','╱',range,print
 for n in r(1,8):
-	a=s*(n*4);b=v+a+v;p(s*(n+1)+o+h*(n*4)+o)
+	a=s*(n*4);b,c=v+a+v,o+h*(n*4)+o;p(s*(n+1)+c)
 	for i in r(n):p(s*(n-i)+d+a+d+" "*i+v)
-	p(o+h*(n*4)+o+(s*n)+v)
+	p(c+(s*n)+v)
 	for i in r(2*n):p([b+s*(2*n-i-1)+d,b+s*n+[v,o][i+1==n]][i<n])
-	p(o+h*(n*4)+o+'\n')
+	p(c+'\n')
