bdcfabff1038f638440f9dbf87c1874dfdd57872
Franklin <franklinjiachen@gmail.com>
2024-07-02 16:46:59 -0700

    [235/227] condense to one loop

--- cubes/solution.py
+++ cubes/solution.py
@@ -1,7 +1,5 @@
 s,o,h,v,d,r,p=' ','█','─','│','╱',range,print
 for n in r(1,8):
 	a=s*(n*4);b,c=v+a+v,o+h*(n*4)+o;p(s*(n+1)+c)
-	for i in r(n):p(s*(n-i)+d+a+d+" "*i+v)
-	p(c+(s*n)+v)
-	for i in r(2*n):p([b+s*(2*n-i-1)+d,b+s*n+[v,o][i>n-2]][i<n])
+	for i in r(3*n):p([s*(n-i)+d+a+d+" "*i+v,(c+s*n+v+'\n')*(i==n)+b+s*n+[v,o][i>2*n-2],b+s*(3*n-i-1)+d][i//n])
 	p(c);p()
