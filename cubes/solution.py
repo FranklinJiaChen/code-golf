b3c931c58b0ca2446672c764def877b37eba9a9b
Franklin <franklinjiachen@gmail.com>
2024-07-02 16:41:13 -0700

    replace +1== with > -2

--- cubes/solution.py
+++ cubes/solution.py
@@ -3,5 +3,5 @@ for n in r(1,8):
 	a=s*(n*4);b,c=v+a+v,o+h*(n*4)+o;p(s*(n+1)+c)
 	for i in r(n):p(s*(n-i)+d+a+d+" "*i+v)
 	p(c+(s*n)+v)
-	for i in r(2*n):p([b+s*(2*n-i-1)+d,b+s*n+[v,o][i+1==n]][i<n])
+	for i in r(2*n):p([b+s*(2*n-i-1)+d,b+s*n+[v,o][i>n-2]][i<n])
 	p(c);p()
