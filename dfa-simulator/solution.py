0e24168bda9e08ea6d169adb40bec83a3abef84f
Franklin <franklinjiachen@gmail.com>
2024-07-03 19:43:19 -0700

    [-20 byte] condense lines

--- dfa-simulator/solution.py
+++ dfa-simulator/solution.py
@@ -1,17 +1,9 @@
 import sys
 for a in sys.argv[1:]:
-	q=[]
-	d={}
-	s,*l,p=a.split("\n")
+	q=[];d={};s,*l,p=a.split("\n")
 	for r,(e,f,n,*t)in enumerate(l):
-		q.append({})
-		if e=='>':
-			c=q[r]
-			b=n
-		q[r]['']=['Reject','Accept'][f=='F']
-		for i,u in enumerate(t[1::2]):
-			q[r][s.split()[i]]=u
-		d[n]=r
-	for j in p[1:-1]:
-		c=q[d[b:=c[j]]]
+		q.append({});d[n]=r;q[r]['']=['Reject','Accept'][f=='F']
+		if e=='>':c=q[r];b=n
+		for i,u in enumerate(t[1::2]):q[r][s.split()[i]]=u
+	for j in p[1:-1]:c=q[d[b:=c[j]]]
 	print(b,c[''])
