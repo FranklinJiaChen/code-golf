93359fa1246713a231fd3e5a7e610739715648bc
Franklin <franklinjiachen@gmail.com>
2024-07-03 19:52:28 -0700

    [-4 bytes]: set enumerate to a variable

--- dfa-simulator/solution.py
+++ dfa-simulator/solution.py
@@ -1,9 +1,9 @@
-import sys
+import sys;E=enumerate
 for a in sys.argv[1:]:
 	q=[];d={};s,*l,p=a.split("\n")
-	for r,(e,f,n,*t)in enumerate(l):
+	for r,(e,f,n,*t)in E(l):
 		q.append({});d[n]=r;q[r]['']=['Reject','Accept'][f>'E']
 		if e>'<':c=q[r];b=n
-		for i,u in enumerate(t[1::2]):q[r][s.split()[i]]=u
+		for i,u in E(t[1::2]):q[r][s.split()[i]]=u
 	for j in p[1:-1]:c=q[d[b:=c[j]]]
 	print(b,c[''])
