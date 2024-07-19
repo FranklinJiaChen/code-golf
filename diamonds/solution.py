c8e1be72a1b8eb8bf5ebd8931475e55e9e604f21
Franklin <franklinjiachen@gmail.com>
2024-07-19 11:54:13 -0700

    -4 bytes p=print

--- diamonds/solution.py
+++ diamonds/solution.py
@@ -1,4 +1,5 @@
+p=print
 for i in range(9):
-	for j in range(i+1):print(str(int('1'*(j+1))**2).center(19))
-	for j in range(i-1,-1,-1):print(str(int('1'*(j+1))**2).center(19))
-	print()
+	for j in range(i+1):p(str(int('1'*(j+1))**2).center(19))
+	for j in range(i-1,-1,-1):p(str(int('1'*(j+1))**2).center(19))
+	p()
