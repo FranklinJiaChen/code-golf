d847c07f0be17a9aef5b69c08ccb08c85b6c4187
Franklin <franklinjiachen@gmail.com>
2024-07-19 12:05:04 -0700

    -2 bytes remove parenthesis with boolean accessing

--- diamonds/solution.py
+++ diamonds/solution.py
@@ -1,4 +1,3 @@
-p=print
 for i in range(9):
-	for j in range(2*i+1):p(str(int('1'*([j+1,2*i-j][j>i]))**2).center(19))
-	p()
+	for j in range(2*i+1):print(str(int('1'*[j+1,2*i-j+1][j>i])**2).center(19))
+	print()
