49edafd5579967a629e2bd9e257ebccd504a0841
Franklin <franklinjiachen@gmail.com>
2024-07-19 12:20:59 -0700

    [-2 bytes] factor out a +1 with -~

--- diamonds/solution.py
+++ diamonds/solution.py
@@ -1,3 +1,3 @@
 for i in range(9):
-	for j in range(2*i+1):print(str(int('1'*[j+1,2*i-j+1][j>i])**2).center(19))
+	for j in range(2*i+1):print(str(int('1'*-~[j,2*i-j][j>i])**2).center(19))
 	print()
