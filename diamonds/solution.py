163d8cf2d8cfd0b1886f3ffa5e160c43aada2c2f
Franklin <franklinjiachen@gmail.com>
2024-07-19 12:02:00 -0700

    -47 bytes (combine for loops)

--- diamonds/solution.py
+++ diamonds/solution.py
@@ -1,5 +1,4 @@
 p=print
 for i in range(9):
-	for j in range(i+1):p(str(int('1'*(j+1))**2).center(19))
-	for j in range(i-1,-1,-1):p(str(int('1'*(j+1))**2).center(19))
+	for j in range(2*i+1):p(str(int('1'*([j+1,2*i-j][j>i]))**2).center(19))
 	p()
