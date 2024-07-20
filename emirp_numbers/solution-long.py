62a4a1543f50ba3df80cfa3a33be736e6bdebba8
Franklin <franklinjiachen@gmail.com>
2024-07-19 18:38:08 -0700

    -1 byte shorten loop to last emirp number

--- emirp_numbers/solution-long.py
+++ emirp_numbers/solution-long.py
@@ -1,3 +1,3 @@
-for n in range(10001):
+for n in range(9968):
 	r=int(str(n)[::-1])
 	if n!=r and all([n%i for i in range(2,n)]+[r%i for i in range(2,r)]):print(n)
--- emirp_numbers/solution.py
+++ emirp_numbers/solution.py
@@ -1,4 +1,4 @@
 f=range
-for n in f(1001):
+for n in f(992):
 	r=int(str(n)[::-1])
 	if all([n!=r]+[n%i for i in f(2,n)]+[r%i for i in f(2,r)]):print(n)
