9247fd456cc1bb4a02c37daa427456ed491cc8e4
Franklin <franklinjiachen@gmail.com>
2024-07-30 14:32:40 -0700

    [refactor loop] -4 bytes
    unnecessary calculations are made but allows for less indents

--- factorial-factorisation/solution.py
+++ factorial-factorisation/solution.py
@@ -1,7 +1,6 @@
 s=""
 for i in range(2,998):
 	f=0;n=1000
-	if all(i%j for j in range(2,i)):
-		while n:f+=(n:=n//i)
-		s+=[f"{i}*",f"{i}^{f}*"][f>1]
+	while n:f+=(n:=n//i)
+	if all(i%j for j in range(2,i)):s+=[f"{i}*",f"{i}^{f}*"][f>1]
 print(s[:-1])
