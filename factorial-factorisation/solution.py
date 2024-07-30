ab7e289861e0133d5451f28084afe577c6344d98
Franklin <franklinjiachen@gmail.com>
2024-07-29 21:54:23 -0700

    update dictionary logic, use division and remove space

--- factorial-factorisation/solution.py
+++ factorial-factorisation/solution.py
@@ -1,11 +1,10 @@
 d={}
 for i in range(1001):
-	j = 2
+	j=2
 	while i>1:
 		if i%j<1:
-			i//=j
-			if j in d:d[j]+=1
-			else:d[j]=1
+			if j not in d:d[j]=0
+			i/=j;d[j]+=1
 		else:j+=1
 s=""
 for k,v in d.items():s+=[f"{k}*",f"{k}^{v}*"][v>1]
