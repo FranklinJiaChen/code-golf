8e8b4248c2ae08f300a001402d5262420776bdc4
Franklin <franklinjiachen@gmail.com>
2024-07-30 14:38:34 -0700

    [-3 byte] replace if: str catenate with boolean multiplication

--- factorial-factorisation/solution.py
+++ factorial-factorisation/solution.py
@@ -2,5 +2,5 @@ s=""
 for i in range(2,998):
 	f=0;n=1000
 	while n:f+=(n:=n//i)
-	if all(i%j for j in range(2,i)):s+=[f"{i}*",f"{i}^{f}*"][f>1]
+	s+=[f"{i}*",f"{i}^{f}*"][f>1]*all(i%j for j in range(2,i))
 print(s[:-1])
