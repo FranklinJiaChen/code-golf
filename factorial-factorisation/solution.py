75e231752003d97fe4e7bcec2be075f863677f39
Franklin <franklinjiachen@gmail.com>
2024-07-29 21:36:48 -0700

    unrefactor string and codegolf [-164 bytes]

--- factorial-factorisation/solution.py
+++ factorial-factorisation/solution.py
@@ -1,17 +1,12 @@
 d={}
 for i in range(1001):
-    j = 2
-    while i>1:
-        if i%j<1:
-            i//=j
-            if j in d:
-                d[j]+=1
-            else:
-                d[j]=1
-        else:
-            j+=1
-
-s = ""
-for key, value in d.items():
-    s+=f"{key}"+["",f"^{value}"][value>1]+"*"
+	j = 2
+	while i>1:
+		if i%j<1:
+			i//=j
+			if j in d:d[j]+=1
+			else:d[j]=1
+		else:j+=1
+s=""
+for k,v in d.items():s+=[f"{k}*",f"{k}^{v}*"][v>1]
 print(s[:-1])
