16bc2df8efc001038b63173c4aaa70d586b01615
Franklin <franklinjiachen@gmail.com>
2024-07-29 19:28:54 -0700

    update factorial factorisation

--- factorial-factorisation/solution.py
+++ factorial-factorisation/solution.py
@@ -2,20 +2,16 @@ d={}
 for i in range(1001):
     j = 2
     while i>1:
-        if i % j == 0:
-            i //= j
+        if i%j<1:
+            i//=j
             if j in d:
-                d[j] += 1
+                d[j]+=1
             else:
-                d[j] = 1
+                d[j]=1
         else:
             j+=1
 
 s = ""
 for key, value in d.items():
-    if value > 1:
-        s += f"{key}^{value}*"
-    else:
-        s += f"{key}*"
-
+    s+=[f"{key}*",f"{key}^{value}*"][value>1]
 print(s[:-1])
