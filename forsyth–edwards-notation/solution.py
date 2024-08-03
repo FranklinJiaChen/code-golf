1684c9c4660d7e9d5987a18e478594e21e40a1b6
Franklin <franklinjiachen@gmail.com>
2024-08-02 22:19:02 -0700

    [-12 bytes] use try except

--- forsyth–edwards-notation/solution.py
+++ forsyth–edwards-notation/solution.py
@@ -2,7 +2,7 @@ import sys
 for a in sys.argv[1:]:
 	for row in a.split()[0].split('/'):
 		for cell in row:
-			if cell.isdigit():print(' '*int(cell),end='')
-			else:print('♔♕♖♗♘♙♚♛♜♝♞♟'['KQRBNPkqrbnp'.index(cell)],end='')
+			try:print('♔♕♖♗♘♙♚♛♜♝♞♟'['KQRBNPkqrbnp'.index(cell)],end='')
+			except:print(' '*int(cell),end='')
 		print()
 	print()
