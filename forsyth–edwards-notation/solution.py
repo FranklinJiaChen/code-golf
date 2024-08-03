edf47460c57b401821434e90935a2aede83e53fa
Franklin <franklinjiachen@gmail.com>
2024-08-03 09:02:27 -0700

    [-1 byte] 1 for loop (list comprehension)

--- forsyth–edwards-notation/solution.py
+++ forsyth–edwards-notation/solution.py
@@ -1,6 +1,5 @@
 import sys
 p=print
 for a in sys.argv[1:]:
-	for c in a.split()[0]:
-		p('♔♕♖♗♘♙♚♛♜♝♞♟'['KQRBNPkqrbnp'.find(c)]*c.isalpha()or'\n'*(c<'0')or' '*int(c),end='')
+	[p('♔♕♖♗♘♙♚♛♜♝♞♟'['KQRBNPkqrbnp'.find(c)]*c.isalpha()or'\n'*(c<'0')or' '*int(c),end='')for c in a.split()[0]]
 	p();p()
