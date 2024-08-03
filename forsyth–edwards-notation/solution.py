71b505abadb4a1f62aed349c6afdc9b147787484
Franklin <franklinjiachen@gmail.com>
2024-08-02 23:17:24 -0700

    Get rid of continue with another try except block

--- forsyth–edwards-notation/solution.py
+++ forsyth–edwards-notation/solution.py
@@ -2,7 +2,8 @@ p=print
 import sys
 for a in sys.argv[1:]:
 	for c in a.split()[0]:
-		if c<'0':p();continue
 		try:p('♔♕♖♗♘♙♚♛♜♝♞♟'['KQRBNPkqrbnp'.index(c)],end='')
-		except:p(' '*int(c),end='')
+		except:
+			try:p(' '*int(c),end='')
+			except:p()
 	p();p()
