07f58ac5026552134cdccd8935d29465c20917f4
Franklin <franklinjiachen@gmail.com>
2024-08-02 22:21:10 -0700

    -8 byte print substitute

--- forsyth–edwards-notation/solution.py
+++ forsyth–edwards-notation/solution.py
@@ -1,8 +1,9 @@
+p=print
 import sys
 for a in sys.argv[1:]:
 	for row in a.split()[0].split('/'):
 		for cell in row:
-			try:print('♔♕♖♗♘♙♚♛♜♝♞♟'['KQRBNPkqrbnp'.index(cell)],end='')
-			except:print(' '*int(cell),end='')
-		print()
-	print()
+			try:p('♔♕♖♗♘♙♚♛♜♝♞♟'['KQRBNPkqrbnp'.index(cell)],end='')
+			except:p(' '*int(cell),end='')
+		p()
+	p()
