5ccba1c4d4505131d33419c93a0d1a7dcc15008f
Franklin <franklinjiachen@gmail.com>
2024-08-03 09:06:44 -0700

    [175 bytes] Use ''.join() for single for loop

--- forsyth–edwards-notation/solution.py
+++ forsyth–edwards-notation/solution.py
@@ -1,5 +1,2 @@
 import sys
-p=print
-for a in sys.argv[1:]:
-	[p('♔♕♖♗♘♙♚♛♜♝♞♟'['KQRBNPkqrbnp'.find(c)]*c.isalpha()or'\n'*(c<'0')or' '*int(c),end='')for c in a.split()[0]]
-	p();p()
+for a in sys.argv[1:]:print(''.join('♔♕♖♗♘♙♚♛♜♝♞♟'['KQRBNPkqrbnp'.find(c)]*c.isalpha()or'\n'*(c<'0')or' '*int(c)for c in a.split()[0])+'\n')
