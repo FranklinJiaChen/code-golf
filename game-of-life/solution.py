c1e60d1d6f067953f3ee71e5cb6fb7709b7ddca5
Franklin <franklinjiachen@gmail.com>
2024-08-03 15:19:02 -0700

    [-10 bytes] one line with ''.join()

--- game-of-life/solution.py
+++ game-of-life/solution.py
@@ -1,9 +1,4 @@
 import sys
-i=sys.argv[1].replace("\n","")
-o=""
-for c in range(1024):
-	k=''
-	for j in[33,32,31,1,-1,-31,-32,-33]:
-		if c+j<999:k+=i[c+j]
-	k=k.count('#');o+=[".","#"][i[c]<'$'and k==2or k==3]+c%32//31*'\n'
+i=sys.argv[1].replace("\n","");o=""
+for c in range(1024):k=''.join(i[c+j]for j in[33,32,31,1,-1,-31,-32,-33]if c+j<999).count('#');o+=[".","#"][i[c]<'$'and k==2or k==3]+c%32//31*'\n'
 print(o)
