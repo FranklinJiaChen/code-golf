af53c783abdb388207a1cb55ca0c0c08bc828a38
Franklin <franklinjiachen@gmail.com>
2024-08-03 15:15:25 -0700

    [212 bytes] exploit dead border

--- game-of-life/solution.py
+++ game-of-life/solution.py
@@ -3,8 +3,7 @@ i=sys.argv[1].replace("\n","")
 o=""
 for c in range(1024):
 	k=''
-	for j in [33,32,31,1,-1,-31,-32,-33]:
-		try:k+=i[c+j]
-		except:1
+	for j in[33,32,31,1,-1,-31,-32,-33]:
+		if c+j<999:k+=i[c+j]
 	k=k.count('#');o+=[".","#"][i[c]<'$'and k==2or k==3]+c%32//31*'\n'
 print(o)
