5abecf73c551d2e65d98dcbd6f087c63acb7ea7b
Franklin <franklinjiachen@gmail.com>
2024-08-03 14:55:58 -0700

    [219] Use try except

--- game-of-life/solution.py
+++ game-of-life/solution.py
@@ -1,5 +1,10 @@
 import sys
 i=sys.argv[1].replace("\n","")
 o=""
-for c in range(1024):k=(c>32and i[c-33]<'$')+(c>31and i[c-32]<'$')+(c>30and i[c-31]<'$')+(c>0and i[c-1]<'$')+(c<1023and i[c+1]<'$')+(c<993and i[c+31]<'$')+(c<992and i[c+32]<'$')+(c<991and i[c+33]<'$');o+=[".","#"][i[c]<'$'and k==2or k==3]+'\n'*(-~c%32<1)
+for c in range(1024):
+	k=''
+	for j in [33,32,31,1,-1,-31,-32,-33]:
+		try:k+=i[c+j]
+		except:1
+	k=k.count('#');o+=[".","#"][i[c]<'$'and k==2or k==3]+'\n'*(-~c%32<1)
 print(o)
