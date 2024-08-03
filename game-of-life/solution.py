f690188b710424ca633cc1d66b74d643c96717d2
Franklin <franklinjiachen@gmail.com>
2024-08-03 14:57:23 -0700

    [-2 bytes] c%32//31*'\n' over '\n'*(-~c%32<1)

--- game-of-life/solution.py
+++ game-of-life/solution.py
@@ -6,5 +6,5 @@ for c in range(1024):
 	for j in [33,32,31,1,-1,-31,-32,-33]:
 		try:k+=i[c+j]
 		except:1
-	k=k.count('#');o+=[".","#"][i[c]<'$'and k==2or k==3]+'\n'*(-~c%32<1)
+	k=k.count('#');o+=[".","#"][i[c]<'$'and k==2or k==3]+c%32//31*'\n'
 print(o)
