ca26f56c83930aeadac2490d511982b735ada256
Franklin <franklinjiachen@gmail.com>
2024-08-03 09:00:15 -0700

    [187 bytes] Remove double try except

--- forsyth–edwards-notation/solution.py
+++ forsyth–edwards-notation/solution.py
@@ -1,9 +1,6 @@
-p=print
 import sys
+p=print
 for a in sys.argv[1:]:
 	for c in a.split()[0]:
-		try:p('♔♕♖♗♘♙♚♛♜♝♞♟'['KQRBNPkqrbnp'.index(c)],end='')
-		except:
-			try:p(' '*int(c),end='')
-			except:p()
+		p('♔♕♖♗♘♙♚♛♜♝♞♟'['KQRBNPkqrbnp'.find(c)]*c.isalpha()or'\n'*(c<'0')or' '*int(c),end='')
 	p();p()
