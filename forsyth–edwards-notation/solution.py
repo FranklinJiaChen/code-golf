0e71301c4b0a447e8eda1929aae3931a0fde16cb
Franklin <franklinjiachen@gmail.com>
2024-08-02 22:29:34 -0700

    [- 1 byte] Use <'0' to check for '/'
    The ascii value happened to work out

--- forsyth–edwards-notation/solution.py
+++ forsyth–edwards-notation/solution.py
@@ -2,7 +2,7 @@ p=print
 import sys
 for a in sys.argv[1:]:
 	for c in a.split()[0]:
-		if c=='/':p();continue
+		if c<'0':p();continue
 		try:p('♔♕♖♗♘♙♚♛♜♝♞♟'['KQRBNPkqrbnp'.index(c)],end='')
 		except:p(' '*int(c),end='')
 	p();p()
