70cba149fce495e3b307e814ff18e632dbeaa1f0
Franklin <franklinjiachen@gmail.com>
2024-08-03 22:18:53 -0700

    [-14 bytes] get rid of replace

--- game-of-life/byte_string.py
+++ game-of-life/byte_string.py
@@ -4,7 +4,7 @@ def list_to_bytes(lst):
 # 32 to 126 are printable ASCII characters
 # 39 and 92 requires escaping in strings and thus 2 bytes.
 # all = [x for x in range(32, 127)]
-lst = (-33, -32, -31, -1, 1, 31, 32, 33)
-lst = [x+65 for x in lst]
+lst = (-34, -33, -32, -1, 1, 32, 33, 34)
+lst = [x+66 for x in lst]
 byte_string = list_to_bytes(lst)
 print(byte_string)
--- game-of-life/solution.py
+++ game-of-life/solution.py
@@ -1,3 +1,3 @@
 import sys
-i=sys.argv[1].replace('\n','');c=0
-for n in i:print('.#'[1+(n>'-')<sum(i[c+j-65]<'$'for j in b' !"@B`ab'if c+j<1088)<4]+c%32//31*'\n',end='');c+=1
+i=sys.argv[1];c=0
+for n in i:print(['.#'[1+(n>'-')<sum('"'<i[c+j-66]<'$'for j in b' !"ACbcd'if c+j<1088)<4],n][n=='\n'],end='');c+=1
