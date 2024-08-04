529620c00ca0e2fced26df67c862a9921adc0c4f
Franklin <franklinjiachen@gmail.com>
2024-08-03 22:10:19 -0700

    [-3 bytes] remove variable o
    Use end='' in print

--- game-of-life/solution.py
+++ game-of-life/solution.py
@@ -1,4 +1,3 @@
 import sys
-i=sys.argv[1].replace('\n','');o='';c=0
-for n in i:o+='.#'[1+(n>'-')<sum(i[c+j-65]<'$'for j in b' !"@B`ab'if c+j<1088)<4]+c%32//31*'\n';c+=1
-print(o)
+i=sys.argv[1].replace('\n','');c=0
+for n in i:print('.#'[1+(n>'-')<sum(i[c+j-65]<'$'for j in b' !"@B`ab'if c+j<1088)<4]+c%32//31*'\n',end='');c+=1
