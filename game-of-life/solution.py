1aa9f631ed988408ee546726c7915f2ad4359761
Franklin <franklinjiachen@gmail.com>
2024-08-03 22:09:01 -0700

    [-4 byte] remove variable k
    only one occurence of variable

--- game-of-life/solution.py
+++ game-of-life/solution.py
@@ -1,4 +1,4 @@
 import sys
 i=sys.argv[1].replace('\n','');o='';c=0
-for n in i:k=sum(i[c+j-65]<'$'for j in b' !"@B`ab'if c+j<1088);o+='.#'[1+(n>'-')<k<4]+c%32//31*'\n';c+=1
+for n in i:o+='.#'[1+(n>'-')<sum(i[c+j-65]<'$'for j in b' !"@B`ab'if c+j<1088)<4]+c%32//31*'\n';c+=1
 print(o)
