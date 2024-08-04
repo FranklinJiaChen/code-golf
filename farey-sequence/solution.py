ef3c8dd2aa235dc1e6d4322cb9aa990e481df093
Franklin <franklinjiachen@gmail.com>
2024-08-04 14:52:25 -0700

    [-8 bytes] double loop one line with list comprehension

--- farey-sequence/solution.py
+++ farey-sequence/solution.py
@@ -1,6 +1,3 @@
-l=[]
-for d in range(1,51):
-	for n in range(d+1):
-		if n/d not in[n for(n,_)in l]:l+=[(n/d,str(n)+'/'+str(d))]
+l=[];[l.append((n/d,f'{n}/{d}'))for d in range(1,51)for n in range(d+1)if n/d not in[n for(n,_)in l]]
 l.sort()
 for i in l:print(i[1])
