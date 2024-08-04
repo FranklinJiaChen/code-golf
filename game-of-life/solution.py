517e487aacd062ee3444c560e24c8a213c8f6b13
Franklin <franklinjiachen@gmail.com>
2024-08-03 22:19:35 -0700

    [-2 bytes] Get rid of =='\n' for <' '

--- game-of-life/solution.py
+++ game-of-life/solution.py
@@ -1,3 +1,3 @@
 import sys
 i=sys.argv[1];c=0
-for n in i:print(['.#'[1+(n>'-')<sum('"'<i[c+j-66]<'$'for j in b' !"ACbcd'if c+j<1088)<4],n][n=='\n'],end='');c+=1
+for n in i:print(['.#'[1+(n>'-')<sum('"'<i[c+j-66]<'$'for j in b' !"ACbcd'if c+j<1088)<4],n][n<' '],end='');c+=1
