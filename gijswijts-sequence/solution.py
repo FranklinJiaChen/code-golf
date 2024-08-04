88c32c8503c999e7a0e8a022085d7e7ac0556399
Franklin <franklinjiachen@gmail.com>
2024-08-04 16:43:50 -0700

    [-2 bytes] put sequence in reverse order

--- gijswijts-sequence/solution.py
+++ gijswijts-sequence/solution.py
@@ -1,6 +1,6 @@
 s='1'
 while len(s)-1001:
-	n=2;print(s[-1])
+	n=2;print(s[0])
 	for i in range(1,len(s)):
-		while s[-i*n:]==s[-i:]*n:n+=1
-	s+=str(~-n)
+		while s[:i*n]==s[:i]*n:n+=1
+	s=str(~-n)+s
