8dcc88db00fa8aacd2692d049678b1f1a0ade65c
Franklin <franklinjiachen@gmail.com>
2024-08-03 21:23:15 -0700

    [-11 bytes]. Not use string and count and keep k as int

--- game-of-life/solution.py
+++ game-of-life/solution.py
@@ -1,4 +1,4 @@
 import sys
 i=sys.argv[1].replace('\n','');o=''
-for c in range(1024):k=''.join(i[c+j]for j in[33,32,31,1,-1,-31,-32,-33]if c+j<999).count('#');o+='.#'[1+(i[c]>'-')<k<4]+c%32//31*'\n'
+for c in range(1024):k=sum(i[c+j]<'$'for j in(33,32,31,1,-1,-31,-32,-33)if c+j<999);o+='.#'[1+(i[c]>'-')<k<4]+c%32//31*'\n'
 print(o)
