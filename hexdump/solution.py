a7e932be6a43e286ee3def4f084b6170ccd689b4
Franklin <franklinjiachen@gmail.com>
2024-08-05 15:05:24 -0700

    [-6 bytes] Replace hex with '%x'%

--- hexdump/solution.py
+++ hexdump/solution.py
@@ -1,7 +1,7 @@
 import sys
 for a in sys.argv[1:]:
 	for i in range(~-len(a)//16+1):
-		s=('0'*7+hex(i*16)[2:])[-8:]+': ';n=0
-		for c in a[i*16:-~i*16]:s+=[hex(ord(c))[2:],'0a'][c=='\n']+' '*(n%2);n+=1
+		s=('0'*7+'%x'%(i*16))[-8:]+': ';n=0
+		for c in a[i*16:-~i*16]:s+=['%x'%ord(c),'0a'][c=='\n']+' '*(n%2);n+=1
 		print(s+' '*(51-len(s))+a[i*16:-~i*16].replace('\n','.'))
 	print()
