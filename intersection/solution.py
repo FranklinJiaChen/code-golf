7d5dddb19451761fd513ca829c3a3929eaf55f57
Franklin <franklinjiachen@gmail.com>
2024-08-05 16:12:16 -0700

    [-3 bytes] h*v*(h>0<v) over boolean access

--- intersection/solution.py
+++ intersection/solution.py
@@ -1,2 +1,2 @@
 import sys
-for A in sys.argv[1:]:a,b,c,d,x,y,w,z=map(int,A.split(' '));h=min(a+c,x+w)-max(a,x);v=min(b+d,y+z)-max(b,y);print([0,h*v][h>0<v])
+for A in sys.argv[1:]:a,b,c,d,x,y,w,z=map(int,A.split(' '));h=min(a+c,x+w)-max(a,x);v=min(b+d,y+z)-max(b,y);print(h*v*(h>0<v))
