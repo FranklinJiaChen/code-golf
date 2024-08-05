a5b9c4a9a08c92e8438bf2ad57b12ff7c1b01bb7
Franklin <franklinjiachen@gmail.com>
2024-08-05 11:53:38 -0700

    [-5 byte] remove unnecessary assignment

--- happy-numbers/solution.py
+++ happy-numbers/solution.py
@@ -1,1 +1,1 @@
-for i in range(201):k=i;exec('s=0;s+=sum([int(d)**2for d in str(k)]);k=s;'*5);0<k<2==print(i)
+for i in range(201):k=i;exec('s=sum([int(d)**2for d in str(k)]);k=s;'*5);0<k<2==print(i)
