7859a683884ce3d8f35e99732c153fe56b4ac827
Franklin <franklinjiachen@gmail.com>
2024-08-05 15:55:14 -0700

    [-17 bytes] while list not empty and shrink list

--- hexdump/solution.py
+++ hexdump/solution.py
@@ -1,7 +1,8 @@
 import sys
 for a in sys.argv[1:]:
-	for i in range(~-len(a)//16+1):
-		s=('0'*7+'%x: '%(i*16))[-10:];p=a[i*16:-~i*16]
+	i=0
+	while p:=a[:16]:
+		a=a[16:];s=('0'*7+'%x: '%i)[-10:]
 		for c in p:s+=len(s)%5//4*' '+['%x'%ord(c),'0a'][c=='\n']
-		print(s+' '*(51-len(s))+p.replace('\n','.'))
+		print(s+' '*(51-len(s))+p.replace('\n','.'));i+=16
 	print()
