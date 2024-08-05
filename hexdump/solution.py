6b7598b27e865b63273fbbf11cf3468bbc0969cd
Franklin <franklinjiachen@gmail.com>
2024-08-05 15:11:44 -0700

    [-9 bytes] assign to variable
    variable a[i*16:-~i*16]

--- hexdump/solution.py
+++ hexdump/solution.py
@@ -1,7 +1,7 @@
 import sys
 for a in sys.argv[1:]:
 	for i in range(~-len(a)//16+1):
-		s=('0'*7+'%x: '%(i*16))[-10:]
-		for c in a[i*16:-~i*16]:s+=len(s)%5//4*' '+['%x'%ord(c),'0a'][c=='\n']
-		print(s+' '*(51-len(s))+a[i*16:-~i*16].replace('\n','.'))
+		s=('0'*7+'%x: '%(i*16))[-10:];p=a[i*16:-~i*16]
+		for c in p:s+=len(s)%5//4*' '+['%x'%ord(c),'0a'][c=='\n']
+		print(s+' '*(51-len(s))+p.replace('\n','.'))
 	print()
