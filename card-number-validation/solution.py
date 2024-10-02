3b49ce2016f64497386ffe16bd04f32e08b16ec4
Franklin <franklinjiachen@gmail.com>
2024-10-01 20:43:13 -0700

    [-5 bytes] short circuit evaluation

--- card-number-validation/solution.py
+++ card-number-validation/solution.py
@@ -1,4 +1,2 @@
 import sys
-for a in sys.argv[1:]:
-	b=a.replace(" ","")
-	if (sum(int(i) for i in b[1::2])+sum(2*(int(i)%5)+(int(i)>4)for i in b[::2]))%10<1:print(a)
+for a in sys.argv[1:]:b=a.replace(" ","");(sum(int(i)for i in b[1::2])+sum(2*(int(i)%5)+(int(i)>4)for i in b[::2]))%10<1==print(a)
